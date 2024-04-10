1d039e158c6a67149e9135b58805a15888f75a4e
Maik Buse <maikbuse@gmail.com>
2024-04-10 20:22:23 +0200

    fix: layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,13 @@ import { BentoGrid } from '@/components/ui/BentoGrid'
 import React from "react";
 import { GoogleGemini } from '@/components/ui/GoogleGemini'
 import CallToAction from '@/components/ui/CallToAction';
-import { Container } from '@/components/Container';
 
 export default async function Home() {
   return (
-    <div className="pt-[5vh]">
+    <div className="pt-[5rem]">
       <Hero />
       <BentoGrid className="mx-auto" />
-      <Container>
-        <GoogleGemini className="h-[250vh] pt-[15rem]" />
-      </Container>
+      <GoogleGemini className="h-[250vh] pt-[15rem]" />
       <CallToAction buttonText='About me' buttonHref='/about' />
     </div >
   )
